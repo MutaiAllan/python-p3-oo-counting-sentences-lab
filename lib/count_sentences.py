b18a65b3c58abbf617f7bb7bbd1f18617346b6e4
MutaiAllan <MutaiAllan>
2023-08-23 17:14:32 +0300

    completed all the methods

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self._value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, new_value):
+    if not isinstance(new_value, str):
+      print("The value must be a string.")
+    self._value = new_value
+
+  def is_sentence(self):
+    if self.value[-1] == '.':
+      return True
+    else:
+      return False
+    
+  def is_question(self):
+    if self.value[-1] == '?':
+      return True
+    else:
+      return False
+    
+  def is_exclamation(self):
+    if self.value[-1] == '!':
+      return True
+    else:
+      return False
+    
+  def count_sentences(self):
+    count = 0
+    sentences = self.value.replace("?", ".").replace("!", ".").replace("...", ".").replace("..", ".")
+    print(sentences)
+    for _ in sentences.split("."):
+      count += 1
+    return count-1
+  
+simple_string = MyString("one. two. three?")
+print(simple_string.count_sentences())
